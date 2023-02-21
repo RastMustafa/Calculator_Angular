e1b99b869d81e170e282b9853f158190a9244865
RastMustafae <mularas78@gmail.com>
2023-02-21 11:02:56 +0300

    styles modificatoin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ScreenComponent } from './screen/screen.component';
+import { KeyboardComponent } from './keyboard/keyboard.component';
+import { OptionsComponent } from './screen/options/options.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScreenComponent,
+    KeyboardComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule
